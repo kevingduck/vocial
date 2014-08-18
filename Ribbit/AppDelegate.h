ab5d0dd2ad5e75c9a985399c440ccd953bc5abe9
Kevin Duck <kevingduck@gmail.com>
2014-08-18 15:17:48 -0700

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Kevin Duck on 8/18/14.
+//  Copyright (c) 2014 Kevin Duck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
